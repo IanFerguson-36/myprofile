2c3e9161fe96b03d69a3ebf32128f08707d635ad
Ian F <ian.ferg36@gmail.com>
2021-09-14 16:05:35 -0600

    divided each section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import Bio from "./components/Bio/Bio";
 import Contact from "./components/Contact/Contact";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Works from "./components/Works/Works";
+import "./App.scss";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <div className="sections">
+        <Bio />
+        <Portfolio />
+        <Works />
+        <Contact />
+      </div>
     </div>
   );
 }
