8b37e961c1c606172b319b2705769638c2f45fa7
Ian F <ian.ferg36@gmail.com>
2021-09-14 14:52:04 -0600

    imported components into app dot js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import Bio from "./components/Bio/Bio";
+import Contact from "./components/Contact/Contact";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Works from "./components/Works/Works";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
