77e2c39ae12cfae7b8eac3860571bb7fa5a89e3a
Ian F <ian.ferg36@gmail.com>
2021-09-20 12:32:12 -0600

    added npm ui icons to dependencies and added items to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import "./Navbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Navbar() {
     return (
         <div className="navbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#bio"> test text</a>
+                    <a href="#bio"className="logo"> Ian Ferguson </a>
+                    <div className="itemcontainer">
+                        <Person className="icon"/>
+                        <span>678-447-9596</span>
+                        </div>
+                        <div className="itemcontainer">
+                        <Mail className="icon"/>
+                        <span>ian.ferg36@gmail.com</span>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                
+                        
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
