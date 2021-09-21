d01d4ba8c8c03fcf21f53291790ef8c02fdcdd55
Ian F <ian.ferg36@gmail.com>
2021-09-21 17:39:47 -0600

    uploaded and stylzed professional headshot

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -4,7 +4,12 @@ import "./Bio.scss"
 export default function Bio() {
     return (
         <div className="bio" id="bio">
-            
+            <div className="left">
+                <div className="imgcontainer">
+                    <img src="./assets/image0.jpeg " alt=""/>
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
