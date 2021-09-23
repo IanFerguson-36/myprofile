a21d3f15501e828f446f67c5e23c0133ea800ce6
Ian F <ian.ferg36@gmail.com>
2021-09-23 12:24:41 -0600

    rough draft of intro page

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -9,7 +9,17 @@ export default function Bio() {
                     <img src="./assets/image0.jpeg " alt=""/>
                 </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there! I'm Ian Ferguson, and I'm a software engineer. This website was created with React.js-
+                        <br></br>
+                            I graduated from the University of Georgia in 2019 with a degree in Entertainment Media Studies. <br /> After a gap year (and surviving covid) I discovered I had a passion for coding and graduated from Georgia Tech's <br />
+                        full stack bootcamp in June of 2021.
+                    </h2>
+                    
+                    
+                </div>
+            </div>
         </div>
     )
 }
