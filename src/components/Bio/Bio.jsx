6390783d5e657102cf95247fb669eb082ac76c68
Ian F <ian.ferg36@gmail.com>
2021-09-28 14:01:48 -0600

    added down arrow from bio, still needs styling

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -11,14 +11,14 @@ export default function Bio() {
             </div>
             <div className="right">
                 <div className="wrapper">
-                    <h2>Hi there! I'm Ian Ferguson, and I'm a software engineer. This website was created with React.js-
+                    <h2>Hi there! I'm Ian Ferguson, and I'm a software engineer. This website was created with React.js. 
                         <br></br>
                             I graduated from the University of Georgia in 2019 with a degree in Entertainment Media Studies. <br /> After a gap year and the pandemic I discovered I had a passion for coding and graduated from <br/> Georgia Tech's 
                         full stack bootcamp in June of 2021.
                     </h2>
                     </div>
                     <a href="#portfolio">
-                        <img src="assets/downarrow.png" alt=""></img>
+                        <img src="assets/downarrow.png" alt="down arrow"></img>
                     </a>
             </div>
         </div>
