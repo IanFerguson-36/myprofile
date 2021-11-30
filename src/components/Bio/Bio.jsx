eb57a51ad3b04814c4777876bad7f34e81c7dee6
Ian F <ian.ferg36@gmail.com>
2021-11-30 12:31:23 -0700

    fixed left side formatting

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -4,7 +4,11 @@ import "./Bio.scss"
 export default function Bio() {
     return (
         <div className="bio" id="bio">
-         <div className="left"></div>
+         <div className="left">
+            <div className="imgContainer">
+                <img src="assets/headshot.png" alt="" />
+            </div>
+         </div>
          <div className="right"></div>
         </div>
     )
